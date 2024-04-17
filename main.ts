1dd78c329c9e718d5adceb15b96c7b20525de21a
Asia-parveen <beautyhealthytips@gmail.com>
2024-04-17 13:16:45 -0700

    added files and folders

---
+++ main.ts
@@ -0,0 +1,19 @@
+
+import inquirer from "inquirer";
+
+const answer:{
+    sentence: string
+}= await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence for count the word:"
+    }
+])
+
+const words =  answer.sentence.trim().split(" ");
+
+console.log(words);
+
+console.log(`Your sentence word count is ${words.length} `);
+
